2bb0324c646d67de203b667b6b51e55ad1d3f4f7
John Haugeland <stonecypher@gmail.com>
2014-05-29 15:08:04 -0700

    working(ish)

--- src/edoc_rebar_frontend.erl
+++ src/edoc_rebar_frontend.erl
@@ -1,7 +1,9 @@
 
 -module(edoc_rebar_frontend).
--export([module/2]).
+-export([module/2, overview/2]).
 
 module(Doc, Options) ->
+    lists:flatten(io_lib:format("~p", [{module, Doc, Options}])).
 
-    lists:flatten(io_lib:format("~p", [{Doc, Options}])).
+overview(A,B) ->
+    lists:flatten(io_lib:format("~p", [{overview, A,B}])).
